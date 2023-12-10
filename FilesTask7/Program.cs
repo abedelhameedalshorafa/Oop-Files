113a3f124bedbf9718db6dae22341dec245b791a
Abedelhameed Alshorafa <abedalshorafa95@gmail.com>
2023-12-10 16:25:09 +0300

    Add project files.

---
+++ FilesTask7/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesTask7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFilePath = "G:\\OrangeTasks\\Oop&Files\\Oop&Files\\readFile.txt";
+            string information = ReadFromFileLineByLine(sourceFilePath);
+            Console.WriteLine(information);
+            Console.WriteLine($"the number of characters is : {getTotalNumberOfCharacters(information)}");
+            Console.WriteLine($"the number of words is : {getTotalNumberOfwords(information)}");
+
+        }
+
+        static int getTotalNumberOfwords(string information)
+        {
+            int count = 0;
+            for (int i = 1; i < information.Length; i++)
+            {
+                if (!char.IsWhiteSpace(information[i - 1]) && char.IsWhiteSpace(information[i]))
+                {
+                    count++;
+                }
+            }
+            if (!char.IsWhiteSpace(information[information.Length - 1]))
+            {
+                count++;
+            }
+            return count;
+        }
+
+        static int getTotalNumberOfCharacters(string information)
+        {
+            int count = 0;
+            for (int i = 0; i < information.Length; i++)
+            {
+                if (char.IsWhiteSpace(information[i]))
+                {
+                    count++;
+                }
+            }
+            return information.Length - count;
+        }
+
+
+        static string ReadFromFileLineByLine(string sourceFilePath)
+        {
+            try
+            {
+                string information = string.Empty;
+                using (StreamReader reader = new StreamReader(sourceFilePath))
+                {
+
+                    while (!reader.EndOfStream)
+                    {
+                        information += " " + reader.ReadLine();
+                    }
+
+                }
+                return information;
+            }
+            catch (Exception ex)
+            {
+                return $"An error occurred: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ Oop&Files/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopTask6
+{
+    class Car
+    {
+        public string make;
+        public int year;
+        public string type;
+        public decimal price;
+        public string color;
+
+        public Car(string make, int year, string type, decimal price, string color)
+        {
+            this.make = make;
+            this.year = year;
+            this.type = type;
+            this.price = price;
+            this.color = color;
+        }
+
+        public void start()
+        {
+            Console.WriteLine("Engine Started");
+        }
+
+        public void stop()
+        {
+            Console.WriteLine("Engine stopped");
+        }
+
+        public virtual string getCarDetails()
+        {
+            return $"{year} {make} {type} - Color: {color}, Price: {price:C}";
+        }
+    }
+
+    class Camry : Car
+    {
+        public string model;
+        public string palletNo;
+
+        public Camry(string make, int year, string type, decimal price, string color, string model, string palletNo)
+            : base(make, year, type, price, color)
+        {
+            this.model = model;
+            this.palletNo = palletNo;
+        }
+
+        public override string getCarDetails()
+        {
+            return base.getCarDetails() + $", Model: {model}, Pallet No: {palletNo}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Camry("Toyota", 2022, "Sedan", 25000.50m, "Blue", "Camry", "123ABC");
+
+            myCar.start();
+            Console.WriteLine(myCar.getCarDetails());
+            myCar.stop();
+        }
+    }
+}
